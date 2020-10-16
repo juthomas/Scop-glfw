bf25965436d85cef403f7be7615b7f81fd9c2493
Julien THOMAS <juthomas@e2r7p20.42.fr>
2020-10-16 13:18:32 +0200

    [ADD] function init

--- src/main.c
+++ src/main.c
@@ -1,48 +1,70 @@
-#include <stdio.h>
-// # include <OpenGL/gl3.h>
-// # include <GLFW/glfw3.h>
+# include <stdio.h>
 # include "../include/scop.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    glViewport(0, 0, width, height);
+	glViewport(0, 0, width, height);
 } 
 
 void processInput(GLFWwindow *window)
 {
-	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, TRUE);
 	}
 }
 
-int main()
+int init(t_scop *scop)
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
+	
+	if (!(scop->win = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL)))
 	{
 		fprintf(stderr,"Failed to create GLFW window");
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window);
+	return (1);
+}
+
+int main()
+{
+	t_scop	scop;
+	//GLFWwindow* window;
+	if (!init(&scop))
+	{
+		return(-1);
+	}
+	// glfwInit();
+	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+
+
+	glfwMakeContextCurrent(scop.win);
 
 	glViewport(0, 0, 800, 600);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetFramebufferSizeCallback(scop.win, framebuffer_size_callback);
 
 
 	printf("Hello World\n");
-	while(!glfwWindowShouldClose(window))
+	while(!glfwWindowShouldClose(scop.win))
 	{
-		processInput(window);
+		processInput(scop.win);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
 		glfwPollEvents();    
-		glfwSwapBuffers(window);
+		glfwSwapBuffers(scop.win);
 	}
 	glfwTerminate();
 	return 0;
