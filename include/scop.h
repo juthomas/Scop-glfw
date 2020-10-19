60ef3d40c92f6f3a7ff70c3ee6076945a97d4775
Julien THOMAS <juthomas@e2r7p20.42.fr>
2020-10-19 12:54:22 +0200

    [ADD] Shader serializer

--- include/scop.h
+++ include/scop.h
@@ -3,9 +3,16 @@
 # define TRUE 1
 # include <OpenGL/gl3.h>
 # include <GLFW/glfw3.h>
-
+# include <fcntl.h>
+# include "../lib/libft/includes/libft.h"
 
 typedef struct	s_scop
 {
-	GLFWwindow	*win;
+	GLFWwindow		*win;
+	int				width;
+	int				height;
+	unsigned int	vao;
+	unsigned int	vbo;
+	unsigned int	shaderProgram;
+
 }				t_scop;
--- src/main.c
+++ src/main.c
@@ -1,71 +1,253 @@
 # include <stdio.h>
 # include "../include/scop.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-} 
 
-void processInput(GLFWwindow *window)
+#define BUFF_SIZE 256
+
+char *str_add(char *s1, char *s2)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	char *tmp;
+
+	if (s1 == NULL)
 	{
-		glfwSetWindowShouldClose(window, TRUE);
+		tmp = ft_strdup(s2);
 	}
+	else
+	{
+		tmp = ft_strjoin(s1, s2);
+	}
+	if (s1)
+	{
+		free(s1);
+	}
+	return (tmp);
 }
 
-int init(t_scop *scop)
+char *open_shader(char *shader_path)
 {
+	int fd;
+	int index;
+	char *shader;
+	char buffer[256];
+
+	shader = NULL;
+	if ((fd = open(shader_path, O_RDONLY)) < 0)
+	{
+		fprintf(stderr, "open err\n");
+	}
+
+	while ((index = read(fd, buffer, BUFF_SIZE)) > 0)
+	{
+		buffer[index]= '\0';
+		shader = str_add(shader, buffer);
+	}
+	return (shader);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char *vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
+
+int main(int argc, char **argv)
+{
+	t_scop scop;
+	char *shader1;
+	if (argc == 2)
+	{
+		shader1 = open_shader(argv[1]);
+		printf("Shader : %s\n", shader1);
+	}
+
+
+	// glfw: initialize and configure
+	// ------------------------------
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	
-	if (!(scop->win = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL)))
+
+
+	// glfw window creation
+	// --------------------
+	scop.win = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (scop.win == NULL)
 	{
-		fprintf(stderr,"Failed to create GLFW window");
+		fprintf(stderr, "Failed to create GLFW window");
 		glfwTerminate();
 		return -1;
 	}
-	return (1);
-}
-
-int main()
-{
-	t_scop	scop;
-	//GLFWwindow* window;
-	if (!init(&scop))
-	{
-		return(-1);
-	}
-	// glfwInit();
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-
-
-
 	glfwMakeContextCurrent(scop.win);
-
-	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(scop.win, framebuffer_size_callback);
 
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	// if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	// {
+	//     std::cout << "Failed to initialize GLAD" << std::endl;
+	//     return -1;
+	// }
 
-	printf("Hello World\n");
-	while(!glfwWindowShouldClose(scop.win))
+
+	// build and compile our shader program
+	// ------------------------------------
+	// vertex shader
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// check for shader compile errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
 	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		fprintf(stderr, "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" );
+	}
+	// fragment shader
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// check for shader compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		fprintf(stderr, "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" );
+	}
+	// link shaders
+	scop.shaderProgram = glCreateProgram();
+	glAttachShader(scop.shaderProgram, vertexShader);
+	glAttachShader(scop.shaderProgram, fragmentShader);
+	glLinkProgram(scop.shaderProgram);
+	// check for linking errors
+	glGetProgramiv(scop.shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(scop.shaderProgram, 512, NULL, infoLog);
+		fprintf(stderr, "ERROR::SHADER::PROGRAM::LINKING_FAILED\n");
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	// float vertices[] = {
+	// 	-0.5f, -0.5f, 0.0f, // left  
+	// 	 0.5f, -0.5f, 0.0f, // right 
+	// 	 0.0f,  0.5f, 0.0f  // top   
+	// }; 
+
+
+	float vertices[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};  
+
+	unsigned int EBO;//
+	glGenBuffers(1, &EBO);//
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &scop.vbo);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+
+	// glBindBuffer(GL_ARRAY_BUFFER, scop.vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, scop.vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0); 
+
+
+	// uncomment this call to draw in wireframe polygons.
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(scop.win))
+	{
+		// input
+		// -----
 		processInput(scop.win);
 
+		// render
+		// ------
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-
-		glfwPollEvents();    
+		// draw our first triangle
+		glUseProgram(scop.shaderProgram);
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// glBindVertexArray(0); // no need to unbind it every time 
+ 
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(scop.win);
+		glfwPollEvents();
 	}
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &scop.vbo);
+	glDeleteProgram(scop.shaderProgram);
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
 	glfwTerminate();
-	return 0;
+	return (0);
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, 1);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
